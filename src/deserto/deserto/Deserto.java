d4a73fd082a285bd4bdec561715f944340465bbd
roman <roman@Gabri>
2017-02-19 16:13:30 +0100

    Primo Commit Gabriele

---
+++ src/deserto/deserto/Deserto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deserto.deserto;
+
+/**
+ *
+ * @author FSEVERI\romano3927
+ */
+public class Deserto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
