3aa1589162acf1dcfefe950fc43f80f3de452a2c
roman <roman@Gabri>
2017-02-27 20:13:02 +0100

    Secondo Commit Gabriele

--- src/deserto/deserto/Battaglia.java
+++ src/deserto/deserto/Battaglia.java
@@ -5,6 +5,13 @@
  */
 package deserto.deserto;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
+import java.net.URL;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,12 +19,67 @@ import javax.swing.JPanel;
  *
  * @author roman
  */
-public class Battaglia extends JFrame{
-    private JFrame secondario;
-    private JPanel psecondario;
-    private JPanel primario;
+public class Battaglia extends JFrame {
+
+    private JFrame Control = new JFrame("Pannello di Controllo");
+    private JFrame Log = new JFrame("Diario di bordo");
+    private JPanel pControl = new JPanel();
+    private JPanel pMain = new Panel();
+    private JPanel pLog = new JPanel();
+    //private Image image;
+
     //ArrayList<JButton> buttons = new ArrayList<JButton>();
     public Battaglia() {
+        //image = Toolkit.getDefaultToolkit().getImage("D:\\roman\\Documents\\NetBeansProjects\\ProgettoDeserto\\ProjectDesertoa/deserto.jpg");
+        this.add(pMain);
+        Log.add(pLog);
+        Control.add(pControl);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Log.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        Control.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        pMain.setVisible(true);
+        pLog.setVisible(true);
+        pControl.setVisible(true);
+        setTitle("BattleGame");
+        setResizable(false);
+        setVisible(true);
+        setSize(705, 408);
+        setLocation(10, 10);
+        Control.setResizable(false);
+        Control.setVisible(true);
+        Control.setSize(500, 350);
+        Control.setLocation(765, 10);
+        Log.setResizable(false);
+        Log.setVisible(true);
+        Log.setSize(1000, 250);
+        Log.setLocation(10, 450);
     }
-    
+
+    class Panel extends JPanel {
+
+        private Image image;
+
+        public Panel() {
+            image = Toolkit.getDefaultToolkit().getImage("D:\\roman\\Documents\\NetBeansProjects\\ProgettoDeserto\\ProjectDesertoa/deserto.jpg");
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.drawImage(image, 20, 20,680,380, this);
+            int start = 20;
+            int num = 35;
+            int numX = 23;
+            for (int i = 0; i < 35; i++) {
+                g2.drawLine(start, 20, start, 700);
+                g2.drawString(""+i+"", 3, num);
+                g2.drawString(""+i+"", numX, 17);
+                g2.drawLine(20, start, 700, start);
+                start += 20;
+                num+=20;
+                numX+=20;
+            }
+        }
+    }
+
 }
--- src/deserto/deserto/Deserto.java
+++ src/deserto/deserto/Deserto.java
@@ -9,6 +9,9 @@ import deserto.carri.normali.CarroLineare;
 import deserto.carri.normali.CarroQuadrato;
 import deserto.carri.speciale.CarroTalpa;
 import deserto.pos.Posizione;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,7 +21,8 @@ import java.util.logging.Logger;
  * @author FSEVERI\romano3927
  */
 public class Deserto {
-
+    public final int ITERTIME=1000;
+    public final int SABOTATO=60000;
     /**
      * @param args the command line arguments
      */
@@ -26,14 +30,14 @@ public class Deserto {
         CarroLineare carroL = new CarroLineare(3, new Posizione(3, 1));
         CarroQuadrato carroQ = new CarroQuadrato(9, new Posizione(12, 7));
         CarroTalpa carroT = new CarroTalpa(5, new Posizione(7, 10));
+        
         System.out.println("Quante proiettili vuoi sparare?");
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        for(int i=0;i<n;i++){
-                
-            System.out.println("Ciao");
-                //new Battaglia();
-                Thread.sleep(1000);
+        int n = 5;//in.nextInt();
+        Battaglia game = new Battaglia();
+        for (int i = 0; i < n; i++) {
+            //System.out.println("Ciao");
+            Thread.sleep(1000);
         }
     }
 
